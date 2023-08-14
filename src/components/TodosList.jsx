ead6c5c6fd94de01992355de282514193df740c5
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-08-14 04:42:19 +0300

    Add to do item component

--- src/components/TodosList.jsx
+++ src/components/TodosList.jsx
@@ -1,9 +1,10 @@
+import TodoItem from '@/components/TodoItem';
 const TodosList = (props) => {
   const { todosProps } = props;
   return (
     <ul>
       {todosProps.map((todo) => (
-        <li>{todo.title}</li>
+        <TodoItem itemProp={todo} />
       ))}
     </ul>
   );
