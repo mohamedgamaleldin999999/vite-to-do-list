c4c17a67d557349a575919b53a2f33173cd25635
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-08-14 04:03:16 +0300

    Add logic content

--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -1,6 +1,23 @@
 const TodosLogic = () => {
+  const todos = [
+    {
+      id: 1,
+      title: 'Setup development environment',
+      completed: true,
+    },
+    {
+      id: 2,
+      title: 'Develop website and add content',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Deploy to live server',
+      completed: false,
+    },
+  ];
   return (
-    <div>TodosLogic content</div>
-  )
-}
+    <div>the content goes here</div>
+  );
+};
 export default TodosLogic;
