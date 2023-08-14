dc719ed643f617b7b887642f36a2e435d39a15ca
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-08-15 01:43:29 +0300

    Render navbar in todoapp

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,9 +1,11 @@
 import Header from '@/components/Header';
 import TodosLogic from '@/components/TodosLogic';
+import Navbar from '@/components/Navbar';
 
 const TodoApp = () => {
   return (
     <>
+      <Navbar />
       <Header />
       <TodosLogic />
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import TodoApp from '@/components/TodoApp';
-import Navbar from '@/components/Navbar';
 
 const domContainer = document.getElementById("root");
 const root = ReactDOM.createRoot(domContainer);
@@ -9,7 +8,6 @@ const root = ReactDOM.createRoot(domContainer);
 root.render(
   <React.StrictMode>
     <TodoApp />
-    <Navbar />
   </React.StrictMode>
 );
 
