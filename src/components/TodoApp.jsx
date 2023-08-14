bad849616fc5aa6d9af7c94e3daf99d2a77c1963
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-08-15 02:40:18 +0300

    Remove nav

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,11 +1,9 @@
 import Header from '@/components/Header';
 import TodosLogic from '@/components/TodosLogic';
-import Navbar from '@/components/Navbar';
 
 const TodoApp = () => {
   return (
     <>
-      <Navbar />
       <Header />
       <TodosLogic />
     </>
